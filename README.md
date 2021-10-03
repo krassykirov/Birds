2ab93d0047cb76a96e57ef43a40435682a9305aa
krassy <krassy.kirov@gmail.com>
2021-10-03 14:33:23 +0300

    fixes

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
 # Birds
-Learning Django Test Website devoted to Birds, still in development
-
-1. python.exe manage.py makemigrations
-2. python.exe manage.py migrate
-3. python.exe manage.py runserver
+Learning Django Test Website devoted to Birds, still in development: https://krassybirds.azurewebsites.net/
 
 ![Piping Plover](https://github.com/krassykirov/Birds/blob/master/static/img/home/123.jpg?raw=true)
--- accounts/settings.py
+++ accounts/settings.py
@@ -65,24 +65,24 @@ WSGI_APPLICATION = 'accounts.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'krassydb',
-        'USER': 'krassy',
-        'PASSWORD': '19',
-        'HOST': 'db',
-        'PORT': 5432,
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME':  str(BASE_DIR / "db/db.sqlite3"),
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'krassydb',
+#         'USER': 'krassy',
+#         'PASSWORD': '19',
+#         'HOST': 'db',
+#         'PORT': 5432,
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':  str(BASE_DIR / "db/db.sqlite3"),
+    }
+}
 
 
 # Password validation
