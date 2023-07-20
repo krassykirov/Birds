ee53b12ea70e1869c5ad5a9c328ea5d33e4123b4
Krasimir Kirov <kirovkr@vmware.com>
2023-07-20 14:36:55 +0300

    helm changes

--- accounts/settings.py
+++ accounts/settings.py
@@ -65,24 +65,22 @@ WSGI_APPLICATION = 'accounts.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'krassydb',
-#         'USER': 'krassy',
-#         'PASSWORD': '19',
-#         'HOST': 'db',
-#         'PORT': 5432,
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':  str(BASE_DIR / "db/db.sqlite3"),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('APP_DB_NAME'),
+        'USER': os.getenv('APP_DB_USER'),
+        'PASSWORD': os.getenv('APP_DB_PASS'),
+        'HOST': os.getenv('DATABASE_HOST', 'db'),
+        'PORT':'5432',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME':  str(BASE_DIR / "db/db.sqlite3"),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
